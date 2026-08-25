5d731c9489f8c8e7ee0aa9ec857192fc9bdf1694
Vahid Tavakkoli <vtavakkoli@yahoo.com>
2026-08-25 19:08:55 +0200

    feat: export UGDB experimental API

--- src/dark_channel_deblur/__init__.py
+++ src/dark_channel_deblur/__init__.py
@@ -4,15 +4,20 @@ from .config import DeblurConfig
 from .consensus import RGACDiagnostics, residual_guided_adaptive_consensus_refine
 from .deblur import deblur_image, estimate_blur_kernel
 from .refinement import annealed_pnp_refine, extreme_channel_refine, reblur_image
+from .ugdb import UGDBDiagnostics, gaussian_linear_update, ugdb_refine, ugdb_restore
 
 __all__ = [
     "DeblurConfig",
     "RGACDiagnostics",
+    "UGDBDiagnostics",
     "annealed_pnp_refine",
     "deblur_image",
     "estimate_blur_kernel",
     "extreme_channel_refine",
+    "gaussian_linear_update",
     "reblur_image",
     "residual_guided_adaptive_consensus_refine",
+    "ugdb_refine",
+    "ugdb_restore",
 ]
-__version__ = "0.5.0"
+__version__ = "0.6.0"
