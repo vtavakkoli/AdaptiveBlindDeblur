fcc46b9536698b12a12fc8642353cf252fbc084d
Vahid Tavakkoli <vtavakkoli@yahoo.com>
2026-08-25 12:17:53 +0200

    Expose RGAC consensus API

--- src/dark_channel_deblur/__init__.py
+++ src/dark_channel_deblur/__init__.py
@@ -1,15 +1,18 @@
 """Adaptive native-resolution blind image deblurring research package."""
 
 from .config import DeblurConfig
+from .consensus import RGACDiagnostics, residual_guided_adaptive_consensus_refine
 from .deblur import deblur_image, estimate_blur_kernel
 from .refinement import annealed_pnp_refine, extreme_channel_refine, reblur_image
 
 __all__ = [
     "DeblurConfig",
+    "RGACDiagnostics",
     "annealed_pnp_refine",
     "deblur_image",
     "estimate_blur_kernel",
     "extreme_channel_refine",
     "reblur_image",
+    "residual_guided_adaptive_consensus_refine",
 ]
-__version__ = "0.4.0"
+__version__ = "0.5.0"
